56853b388166ef978ba709b1624620239ae19a64
softprops <d.tangren@gmail.com>
2017-04-26 10:41:41 -0400

    skip serializing Option::None

--- src/schema.rs
+++ src/schema.rs
@@ -8,24 +8,42 @@ pub struct Spec {
     pub info: Info,
     pub paths: BTreeMap<String, Operations>, // / -> get -> op
     pub definitions: BTreeMap<String, Schema>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub schemes: Option<Vec<String>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub host: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="basePath")]
     pub base_path: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub consumes: Option<Vec<String>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub produces: Option<Vec<String>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub parameters: Option<BTreeMap<String, Parameter>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub responses: Option<BTreeMap<String, Response>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="securityDefinitions")]
     pub security_definitions: Option<BTreeMap<String, Security>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub tags: Option<Vec<Tag>>,
+}
+
+
+impl Spec {
+    pub fn unfurl(self) -> Self {
+        self
+    }
 }
 
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Tag {
     pub name: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub description: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="externalDocs")]
     pub external_docs: Option<Vec<ExternalDoc>>,
 }
@@ -33,6 +51,7 @@ pub struct Tag {
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct ExternalDoc {
     pub url: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub description: Option<String>,
 }
 
@@ -40,27 +59,39 @@ pub struct ExternalDoc {
 pub struct Info {
     pub title: String,
     pub version: String,
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub termsOfService: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Operations {
+    #[serde(skip_serializing_if="Option::is_none")]
     pub get: Option<Operation>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub post: Option<Operation>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub put: Option<Operation>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub patch: Option<Operation>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub delete: Option<Operation>,
 }
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Operation {
+    #[serde(skip_serializing_if="Option::is_none")]
     pub description: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub consumes: Option<Vec<String>>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub produces: Option<Vec<String>>,
     pub schemes: Vec<String>,
     pub tags: Vec<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="operationId")]
     pub operation_id: Option<String>,
     pub responses: BTreeMap<String, Response>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub parameters: Option<Vec<Parameter>>,
 }
 
@@ -70,10 +101,14 @@ pub struct Parameter {
     pub name: String,
     #[serde(rename="in")]
     pub location: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub required: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub schema: Option<Schema>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="uniqueItems")]
     pub unique_items: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="type")]
     pub param_type: Option<String>,
 }
@@ -81,6 +116,7 @@ pub struct Parameter {
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Response {
     pub description: String,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub schema: Option<Schema>,
 }
 
@@ -92,11 +128,16 @@ pub struct Security {
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
 pub struct Schema {
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="$ref")]
     pub ref_path: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub description: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     #[serde(rename="type")]
     pub schema_type: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
     pub items: Option<Box<Schema>>, // if scheme_type array (box is for recursion)
+    #[serde(skip_serializing_if="Option::is_none")]
     pub properties: Option<BTreeMap<String, Schema>>, // implies object
 }
